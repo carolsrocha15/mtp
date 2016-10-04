7db3429d0bfaea30d9a180660d97186ab05c9b92
carolsrocha15 <carolsrocha15@gmail.com>
2016-10-04 10:54:02 -0300

    fim de aula

---
+++ aula20160927/stk2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main() {
+	
+	return 0;
+}
+
+void recebaNumero(float num1, float num2, float num3, float num4, float num5) {
+	printf("Entre com o valor de 5 numeros: ");
+	scanf("%f", &num1);
+	scanf("%f", &num2);
+	scanf("%f", &num3);
+	scanf("%f", &num4);
+	scanf("%f", &num5);
+}
+
+void imprimeMedia
